76e1a63d86b67c41c53c2db5bcdf84c2de0a96a1
abdulrazzaqsheikh <2003razzaq@gmail.com>
2024-05-28 15:58:31 +0500

    first assignment

---
+++ assignment.dart
@@ -0,0 +1,12 @@
+void main() {
+  var length = 50 ;
+  var breath = 40 ;
+  var namearea = length * breath ;
+  print ("area $namearea ") ;
+   
+  num  cel = 35 ;
+    num far = ( cel * 9 /5) + 32 ;
+print ("fahrenhiet ---> $far");
+ }
+ 
+
